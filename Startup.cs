543b48d2dbdf75b6a6b1d505d36114e56925cbb4
Cristian Paolini <cristianpaolini3@gmail.com>
2021-10-27 18:22:35 -0300

    Seeded database.

--- Startup.cs
+++ Startup.cs
@@ -59,6 +59,9 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
