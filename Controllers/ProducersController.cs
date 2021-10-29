267633fb68113c04c396449b9c2dd40450a5ddf0
Cristian Paolini <cristianpaolini3@gmail.com>
2021-10-29 18:35:16 -0300

    Added Producers View.

--- Controllers/ProducersController.cs
+++ Controllers/ProducersController.cs
@@ -18,7 +18,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()    //asynchronous way
         {
             var producersList = await _context.Producers.ToListAsync();
-            return View();
+            return View(producersList);
         }
     }
 }
