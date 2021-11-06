1bbceecf448851162c1ee14fbd14d7e8aecb9253
Cristian Paolini <cristianpaolini3@gmail.com>
2021-11-06 16:51:28 -0300

    Added View and method to Add a new Cinema.

--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -13,10 +13,13 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Cinema logo")]
+        [Required(ErrorMessage = "Cinema logo is required.")]
         public string Logo { get; set; }
         [Display(Name = "Cinema name")]
+        [Required(ErrorMessage = "Cinema name is required.")]
         public string Name { get; set; }
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required.")]
         public string Description { get; set; }
 
         //Relationships
