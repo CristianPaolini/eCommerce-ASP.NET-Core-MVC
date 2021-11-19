d6c1ad5d600f5fa0375a9aa100a19c597b5a9aa9
Cristian Paolini <cristianpaolini3@gmail.com>
2021-11-19 01:22:27 -0300

    Added Login View Model.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTickets.Data;
+using eTickets.Data.ViewModels;
+using eTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eTickets.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser>
+            signInManager, AppDbContext context)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
         }
+        public IActionResult Index() => View(new LoginVM());
     }
 }
